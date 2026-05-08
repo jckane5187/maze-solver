0ca50198812619cafedf6eed30b82e2ef40e3d70
jckane5187 <jckane5187@gmail.com>
2026-05-07 21:32:46 -0500

    built the maze class, which currently builds a grid based on the inputs

--- main.py
+++ main.py
@@ -3,17 +3,11 @@ from window import Window
 from point import Point
 from line import Line
 from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
-    test_cell = Cell(win)
-    test_cell2 = Cell(win)
-    test_cell.draw(10, 20, 10, 20)
-    test_cell2.draw(20, 30, 10, 20)
-    test_cell.draw_move(test_cell2)
-    test_cell3 = Cell(win)
-    test_cell3.draw(20, 30, 20, 30)
-    test_cell2.draw_move(test_cell3)
+    maze = Maze(10, 10, 15, 15, 20, 20, win)
     win.wait_for_close()
 
 
---
+++ maze.py
@@ -0,0 +1,46 @@
+from line import Line
+from point import Point
+from tkinter import Tk, BOTH, Canvas
+import time
+from cell import Cell
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        self.__cells = [[Cell(self.__win) for a in range(self.__num_rows)] for b in range(self.__num_cols)]
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i, j):
+        x1, x2, y1, y2 = 0, 0, 0, 0
+        if i == 0:
+            x1 = self.__x1
+            x2 = x1 + self.__cell_size_x
+        else:
+            x1 = self.__x1 + (i * self.__cell_size_x)
+            x2 = x1 + self.__cell_size_x
+        
+        if j == 0:
+            y1 = self.__y1
+            y2 = y1 + self.__cell_size_y
+        else:
+            y1 = self.__y1 + (j * self.__cell_size_y)
+            y2 = y1 + self.__cell_size_y
+        
+        self.__cells[i][j].draw(x1, x2, y1, y2)
+        self.__animate()
+
+    def __animate(self):
+        self.__win.redraw()
+        time.sleep(.05)
